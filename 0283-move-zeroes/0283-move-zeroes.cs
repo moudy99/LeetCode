f67494cee2b2134d98827b2fd013ba73deed65c2
Moudy <101123413+moudy99@users.noreply.github.com>
2025-03-17 22:19:55 +0200

    Time: 1 ms (96.95%), Space: 58.2 MB (75.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cs
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -1,28 +1,23 @@
 public class Solution {
-    public void MoveZeroes(int[] nums) {
-      if(nums.Length <2) return;
-        int[] newArr= new int[nums.Length];
-        int counter= 0;
-        for (int i = 0; i < nums.Length; i++)
-            {
-                if (nums[i] == 0)
-                {
-                    continue;
-                }
-                else
-                {
+     public void MoveZeroes(int[] nums)
+    {
 
-                newArr[counter] = nums[i];
-                    counter++;
+        int numZeroIndx=0;
+        for(int i=0; i<nums.Length; i++)
+        {
+            if(nums[i] !=0)
+            {
+                nums[numZeroIndx] =nums[i];
+                if(i!= numZeroIndx)
+                {
+                    nums[i]=0;
                 }
-          }
-     for(int i=0; i<nums.Length; i++)
-     {
-        nums[i]=newArr[i];
-     }
+                numZeroIndx++;
+            }
+
+        }
+
     }
+    
 }
 
-// the brude force - >
-/*
-*/
