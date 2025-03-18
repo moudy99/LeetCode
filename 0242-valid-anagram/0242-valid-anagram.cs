c67970dc478bedc9742431f4012ec93b39a0a25e
Moudy <101123413+moudy99@users.noreply.github.com>
2025-03-18 13:42:17 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,20 +1,42 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-  if (s.Length != t.Length)
-  {
-      return false;
-  }
-  for (int i = 0; i < s.Length; i++)
-  {
-      if (t.Contains(s[i]))
-      {
 
-          t = t.Remove(t.IndexOf(s[i]),1);
-      }
-      else
-      {
-          return false;
-      }
-  }
-  return true;
-}}
+// Old solution with char[]; 
+        // char[] charArray1 = s.ToCharArray();
+        // char[] charArray2 = t.ToCharArray();
+
+        // Array.Sort(charArray1);
+        // Array.Sort(charArray2);
+
+        // if(new string(charArray1) == new string(charArray2)) return true;
+        // return false;
+
+// test solution with HashMap (Dictionary)-> 
+
+
+    if(s.Length != t.Length) return false;
+
+    Dictionary<char,int> dic = new Dictionary<char,int>();
+    foreach(char ch in s)
+    {
+        if(dic.ContainsKey(ch))
+        {
+            dic[ch] = dic[ch]++;
+        }else{
+            dic[ch]=1;
+        }
+    }
+
+    foreach(char ch in t)
+    {
+        if(dic.ContainsKey(ch) && dic[ch] >0)
+        {
+            dic[ch] = dic[ch]--;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+}
