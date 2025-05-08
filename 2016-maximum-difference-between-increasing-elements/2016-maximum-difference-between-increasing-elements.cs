f80763c82b6de2d192fbdf79cf28292b96fa68fd
Moudy <101123413+moudy99@users.noreply.github.com>
2025-05-08 23:18:06 +0300

    Time: 0 ms (100%), Space: 41.9 MB (61.9%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cs
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cs
@@ -1,17 +1,29 @@
 public class Solution {
     public int MaximumDifference(int[] nums) {
         int MaxDiff = int.MinValue;
+            // for(int i=0; i<nums.Length; i++)
+            // {
+            //     for(int j= i + 1; j<nums.Length; j++)
+            //     {   
+            //         if(j>i && nums[j] > nums[i]) {
+            //             if(nums[j] - nums[i] > MaxDiff )
+            //             MaxDiff = nums[j] - nums[i];
+            //         }
+            //     }
+            // }
+
+            int MinV =0;
             for(int i=0; i<nums.Length; i++)
             {
-                for(int j= i + 1; j<nums.Length; j++)
-                {   
-                    if(j>i && nums[j] > nums[i]) {
-                        if(nums[j] - nums[i] > MaxDiff )
-                        MaxDiff = nums[j] - nums[i];
-                    }
-                }
-            }
+                if( nums[MinV] > nums[i] ) MinV = i;
 
+                if(nums[MinV] < nums[i] && MinV < i )
+                {
+                    if(nums[i] - nums[MinV] > MaxDiff )
+                    MaxDiff = nums[i] - nums[MinV];
+                }
+
+            }
             return MaxDiff > 0 ? MaxDiff : -1;
     }
 }
