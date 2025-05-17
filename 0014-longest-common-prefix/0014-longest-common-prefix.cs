e35326e907871871f8d0ff1a6f2ae9f58553b4b1
Moudy <101123413+moudy99@users.noreply.github.com>
2025-05-17 12:24:39 +0300

    Time: 0 ms (100%), Space: 43.8 MB (24.86%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,25 +1,41 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-      
-        StringBuilder sb = new StringBuilder(strs[0]);
-        int index = 0;
-        for(int i=0; i<strs.Length; i++)
+
+        //if (strs == null || strs.Length == 0) return ""; 
+        // StringBuilder sb = new StringBuilder(strs[0]);
+        // int index = 0;
+        // for(int i=0; i<strs.Length; i++)
+        // {
+        //     index = 0;
+        //     for(int j=0; j< sb.Length; j++)
+        //     {
+        //         if (  index > strs[i].Length -1 || sb[index] != strs[i][j]) 
+        //         {
+        //             sb.Remove(index, sb.Length - index);
+        //             break;
+        //         }
+        //         else if(sb[index] == strs[i][j])
+        //         {
+        //             index++;
+        //         }
+        //     }
+        // }
+        // return sb.ToString();
+
+        // update and optmized solution ->
+
+         if (strs == null || strs.Length == 0) return ""; 
+
+        string common = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
         {
-            index = 0;
-            for(int j=0; j< sb.Length; j++)
+            while (!strs[i].StartsWith(common))
             {
-                if (  index > strs[i].Length -1 || sb[index] != strs[i][j]) 
-                {
-                    sb.Remove(index, sb.Length - index);
-                    break;
-                }
-                else if(sb[index] == strs[i][j])
-                {
-                    index++;
-                }
+                common = common.Substring(0, common.Length - 1);
+                if (common == "") return "";
             }
         }
-        return sb.ToString();
-
+        return common;
     }
 }
